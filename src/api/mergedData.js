8cadb692c93db95036cd600736768006d6775337
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-05-28 20:48:15 -0500

    Added createBook function to connect to form

--- src/api/mergedData.js
+++ src/api/mergedData.js
@@ -1,5 +1,5 @@
-import { getSingleBook } from './bookData';
-import { getbooksByAuthor, getSingleAuthor } from './authorData';
+import { deleteBook, getSingleBook } from './bookData';
+import { deleteSingleAuthor, getbooksByAuthor, getSingleAuthor } from './authorData';
 
 const viewBookDetails = (bookFirebaseKey) => new Promise((resolve, reject) => {
   getSingleBook(bookFirebaseKey)
@@ -21,7 +21,19 @@ const viewAuthorDetails = (authorFirebaseKey) => new Promise((resolve, reject) =
     }).catch((error) => reject(error));
 });
 
+const deleteAuthorBooks = (authorId) => new Promise((resolve, reject) => {
+  getbooksByAuthor(authorId).then((booksArray) => {
+    console.warn(booksArray, 'Author Books');
+    const deleteBookPromises = booksArray.map((book) => deleteBook(book.firebaseKey));
+
+    Promise.all(deleteBookPromises).then(() => { /* we don't need anything in then() because we aren't doing anything with the deleted books */
+      deleteSingleAuthor(authorId).then(resolve);
+    // deleteSingleAuthor(authorId).then((response) =>resolve(response));
+    });
+  }).catch((error) => reject(error));
+});
 export {
   viewBookDetails,
-  viewAuthorDetails
+  viewAuthorDetails,
+  deleteAuthorBooks
 };
